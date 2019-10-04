d098c96f673cb46e75e0e45af9f0fe445616e3fd
Akhila Joshi <akhilajo25@gmail.com>
2019-10-04 16:06:49 +0530

    Add files via upload

---
+++ JS_Stalker_Exercise/script.js
@@ -0,0 +1,12 @@
+var firstName = prompt("Enter your First Name");
+
+var lastName = prompt("Enter your Last Name");
+
+var age = prompt("Enter your Age");
+
+alert("Hello, " +firstName+" "+lastName);
+
+alert("Your age, " +age);
+
+
+
