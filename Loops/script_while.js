d18c6fb0a4b125536fda12bf840b8cee424d554c
akhilajo25 <akhilajo25@gmail.com>
2019-10-09 13:32:03 +0530

    JavaScript Exercises

--- Loops/script_while.js
+++ Loops/script_while.js
@@ -8,3 +8,30 @@ while(num<=19){
 	console.log(num);
 	num++;
 }
+
+// Printing all even Numbers between 10 to 20
+
+console.log("Printing all even Numbers between 10 to 20");
+
+var num_even=10
+
+while(num_even<=20){
+	if(num_even%2==0){
+		console.log(num_even);
+	}
+	num_even++;
+}
+
+// Print all odd numbers betweeen 300 t0 333
+
+
+console.log("Printing all odd Numbers between 300 to 333");
+
+var num_odd=300
+
+while(num_odd<=333){
+	if(num_odd%2==1){
+		console.log(num_odd);
+	}
+	num_odd++;
+}
