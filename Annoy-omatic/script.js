61d6c5e4e4e7b4373e788a9367f00907926be422
akhilajo25 <akhilajo25@gmail.com>
2019-10-09 18:51:10 +0530

    JavaScript Exercises

---
+++ Annoy-omatic/script.js
@@ -0,0 +1,19 @@
+
+// var answer = prompt("Are we there yet?");
+
+// while(answer!=="yes" && answer!=="yeah" ){
+
+// 	var answer = prompt("Are we there yet?");
+// }
+
+// alert("Yay! We made it.");
+
+//Version----indexOf
+var answer = prompt("Are we there yet?");
+
+while(answer.indexOf("yes") === -1 ){
+
+	var answer = prompt("Are we there yet?");
+}
+
+alert("Yay! We made it.");
