37a3c1f96ad7e9291824bd43b525189019750dda
akhilajo25 <akhilajo25@gmail.com>
2019-10-18 14:11:26 +0530

    Used Chart.js library to plot line graph

--- Weather/script.js
+++ Weather/script.js
@@ -1,6 +1,6 @@
 //alert("Connected"); Making Sure Everything is connected
 var appId = 'b8779202d4a0cf1d8b4c7047a3953d6c';
-var units = 'imperial';
+var units = 'metric';
 
 var searchMethod='';
 var notFoundHeader = document.getElementById('notFound');
@@ -14,7 +14,7 @@ function getSearch(query){
 
 function searchWeather(query){
 	getSearch(query);
-	fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${query}&APPID=${appId}&units=${units}`).then(result =>{
+	fetch(`http://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${query}&APPID=${appId}&units=${units}&cnt=7`).then(result =>{
 		return result.json();
 	}).then(result=>{
 		init(result);
@@ -23,7 +23,7 @@ function searchWeather(query){
 }
 
 function init(resultFromServer){
-	console.log(typeof resultFromServer["cod"]);
+	console.log(resultFromServer);
 	if(resultFromServer["cod"] == 404 ){
 		document.body.style.backgroundImage="url('default.jpg')";
 		var weatherContainer = document.getElementById('weatherContainer');
@@ -31,7 +31,19 @@ function init(resultFromServer){
 		weatherContainer.style.visibility = 'hidden';
 	}else{
 	notFoundHeader.style.visibility = 'hidden';
-	switch(resultFromServer.weather[0].main){
+	var tempratureArray=[];
+	var sumWindSpeed=0;
+	var sumTemprature=0;
+	for(var i=0;i<resultFromServer["list"].length ; i++)
+	{
+
+	tempratureArray.push(resultFromServer["list"][i]["main"]["temp"]);
+	sumWindSpeed+=(resultFromServer["list"][i]["wind"]["speed"]);
+	sumTemprature+=(resultFromServer["list"][i]["main"]["temp"]);
+	}
+	console.log(sumTemprature);
+	plotGraph(tempratureArray);
+/*	switch(resultFromServer.weather[0].main){
 		case 'Clouds':
 			document.body.style.backgroundImage="url('clouds.jpg')";
 			break;
@@ -54,7 +66,7 @@ function init(resultFromServer){
 		default:
 			document.body.style.backgroundImage="url('default.jpg')";
 
-			break;
+			break;*/
 	}
 	
 	var weatherDescHeader = document.getElementById('weatherDescHeader');
@@ -64,25 +76,25 @@ function init(resultFromServer){
 	var windSpeed = document.getElementById('windSpeed');
 	var weatherIcon = document.getElementById('documentIconImg');
 	
-	weatherIcon.src = "http://openweathermap.org/img/w/"+ resultFromServer.weather[0].icon+".png";
+/*	weatherIcon.src = "http://openweathermap.org/img/w/"+ resultFromServer.weather[0].icon+".png";
 	var resultDesc = resultFromServer.weather[0].description;
 	console.log(resultDesc);
-	weatherDescHeader.innerText = resultDesc.charAt(0).toUpperCase()+resultDesc.slice(1);	
-	temprature.innerHTML = Math.floor(resultFromServer.main.temp)+'&#176';
+	weatherDescHeader.innerText = resultDesc.charAt(0).toUpperCase()+resultDesc.slice(1);*/
+	temprature.innerHTML ="Average Temprature " + Math.floor((sumTemprature)/7)+'&#176';
 
-	windSpeed.innerHTML = "Wind at " + Math.floor(resultFromServer.wind.speed) + ' m/s';
-	cityHeader.innerHTML = resultFromServer.name;
-	humidity.innerHTML = 'Humidity levels at '+ resultFromServer.main.humidity+'%'
+	windSpeed.innerHTML = "Average wind " + Math.floor((sumWindSpeed)/7) + ' m/s';
+	cityHeader.innerHTML = resultFromServer.city.name;
+	/*humidity.innerHTML = 'Humidity levels at '+ resultFromServer.main.humidity+'%'*/
 	setPosition();
 }
-}
+/*}*/
 
 function setPosition(){
 	var weatherContainer = document.getElementById('weatherContainer');
 	var weatherContainerHeight = weatherContainer.clientHeight;
 	var weatherContainerWidth = weatherContainer.clientWidth;
 	weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
-	weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+	weatherContainer.style.top = `calc(52% - ${weatherContainerHeight/2}px)`;
 	weatherContainer.style.visibility = 'visible';
 }
 
@@ -97,3 +109,32 @@ document.getElementById('searchInput').addEventListener('keyup',function(e){
 			}
 	}
 })
+
+var myChart=null;
+function plotGraph(tempratureArray){
+if(myChart!=null){
+        myChart.destroy();
+    }
+
+Chart.defaults.global.defaultFontColor='white';
+var day = [1,2,3,4,5,6,7];
+var ctx = document.getElementById("myChart");
+myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: day,
+    datasets: [
+      { 
+        data: tempratureArray,
+        label:"Temprature in C",
+        borderColor: "white",
+		fill: false
+      }
+    ]
+  },
+  options:{
+			responsive: true  
+        }
+});
+
+}
